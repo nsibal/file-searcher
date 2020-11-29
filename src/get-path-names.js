aef9629028d925f3b44ad267ce42f73d1c8cf5af
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-11-29 16:32:37 -0600

    Create get-path-names.js

---
+++ src/get-path-names.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+// ---------- getPathNames properties ------------------
+/*
+Input: String
+Output: [String] -> contains directory names eg. 'dir/file.txt'
+*/
+
+function getPathNames (dir) {
+	return new Promise((resolve, _) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        resolve([]);
+      } else {
+        resolve(files);
+      }
+    });
+  });
+}
