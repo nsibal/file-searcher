f02ebea2084d95e5b50d82e8f4988a99522e846f
Nirbhay Sibal <nirbhay.sibal@match.com>
2020-11-29 16:57:40 -0600

    Create get-nested-path-names.js

---
+++ src/get-nested-path-names.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const getPathNames = require('./get-path-names.js');
+
+// ---------- getNestedPathNames properties ------------------
+/*
+Input: String
+Output: [String] -> contains all possible directory names
+*/
+async function getNestedPathNames (dir) {
+  let dirNames = await getPathNames(dir);
+  
+	if (dirNames.length === 0) return [];
+
+	let nestedPathPromises = dirNames.map(dir => getNestedPathNames(dir));
+	let nestedPath = await Promise.all(nestedPathPromises);
+
+	return generateFlatList(dirNames, nestedPath);
+}
+
+function generateFlatList (dirNames, nestedPath) {
+  let nestedPathNames = new Array();
+
+	dirNames.forEach(dir => nestedPathNames.push(dir));
+
+	for (let path of nestedPath) {
+		path.forEach(loc => nestedPathNames.push(loc));
+	}
+
+	return nestedPathNames;
+}
--- src/get-path-names.js
+++ src/get-path-names.js
@@ -1,17 +1,17 @@
-const fs = require('fs')
+const fs = require('fs');
+
 // ---------- getPathNames properties ------------------
 /*
 Input: String
 Output: [String] -> contains directory names eg. 'dir/file.txt'
 */
-
-function getPathNames (dir) {
+module.exports = function getPathNames (dir) {
 	return new Promise((resolve, _) => {
     fs.readdir(dir, (err, files) => {
       if (err) {
         resolve([]);
       } else {
-        resolve(files);
+        resolve(files.map(file => `${dir}/${file}`));
       }
     });
   });
